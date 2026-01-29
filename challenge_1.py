85746da1e439c3ea2be835d471533492a2ef4b59
Gani-1395 <aluruganeshreddy96@gmail.com>
2026-01-29 20:03:48 +0530

    Edited day 1 program

--- challenge_1.py
+++ challenge_1.py
@@ -6,7 +6,7 @@ age = int(input("Enter your age:"))
 if name[0] != " " and name[len(name)-1] != " " and " " in name:
     if email[0] != "@" and email.count("@") == 1 and "." in email:
         if number[0] != "0" and len(number) == 10 and number.isdigit():
-            if 18 <= age <= 80:
+            if 18 <= age <= 60:
                 print("User Profile is VALID")
             else:
                 print("User Profile is INVALID")
