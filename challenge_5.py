9df0f5e984c9392e8fd2934cd71afc07b594b0f1
Gani-1395 <aluruganeshreddy96@gmail.com>
2026-02-17 19:25:03 +0530

    logic for implementing load classification

--- challenge_5.py
+++ challenge_5.py
@@ -4,5 +4,27 @@ if n.isdigit():
     data = [0] * m
     for i in range(m):
         data[i] = int(input())
+        very_light = [0] * m
+        normal_load = [0] * m
+        heavy_load = [0] * m
+        overload = [0] * m
+        invalid_entries = [0] * m
+        vl = nl = hl = ol = inv = 0
+        for i in range(m):
+            if data[i] < 0:
+                invalid_entries[inv] = data[i]
+                inv += 1
+            elif 0 <= data[i] <= 5:
+                very_light[vl] = data[i]
+                vl += 1
+            elif 6 <= data[i] <= 25:
+                normal_load[nl] = data[i]
+                nl += 1
+            elif 26 <= data[i] <= 60:
+                heavy_load[hl] = data[i]
+                hl += 1
+            else:
+                overload[ol] = data[i]
+                ol += 1
 else:
     print("invalid number")
