9153f110f23668d7bd3c3f6d3edbef6753373588
Gani-1395 <aluruganeshreddy96@gmail.com>
2026-04-23 13:56:43 +0530

    Delete ext_1.py

--- ext_1.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter the number of entries: "))
-b = int(input("Enter the divisor: "))
-if b == 0:
-    print("Divisor cannot be zero!")
-else:
-    a = [x / b for x in range(n) if x.is_integer()]
-    print(a)
